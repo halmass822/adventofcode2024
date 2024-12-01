3fa3e10c60b8de21fedc230e358eea6b3b02fe0b
halmass822 <halmassri822@gmail.com>
2024-12-01 15:06:33 -0500

    day 1 part 2

--- 1/1.js
+++ 1/1.js
@@ -12,17 +12,27 @@ const rightList = [];
 
 rl.on('line', (line) => {
     const IDs = line.match(/\d+/g).map(x => +x);
-    console.log(IDs);
     leftList.push(IDs[0]);
     rightList.push(IDs[1]);
 })
 
 rl.on('close', () => {
+    
+    //Part 1
+
     let counter = 0;
     const sortedLeft = leftList.sort();
     const sortedRight = rightList.sort();
     for(let i in sortedLeft) {
         counter += Math.abs(sortedLeft[i] - sortedRight[i]);
     };
-    console.log(counter);
+    console.log("p1: ", counter);
+
+    //Part 2
+    let counter2 = 0;
+    for(let i in leftList) {
+        counter2 += leftList[i] * rightList.filter((x) => x === leftList[i]).length
+    };
+    console.log("p2: ", counter2);
+
 })
