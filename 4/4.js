d066c510db11f0041c36494de183de3567284ba6
halmass822 <93576140+halmass822@users.noreply.github.com>
2024-12-09 17:57:19 +0000

    day 4 part 1, vertical and horizontal count complete

--- 4/4.js
+++ 4/4.js
@@ -1,7 +1,7 @@
 const fs = require("fs");
 const readline = require("readline");
 
-const filepath = "./4.txt";
+const filepath = "./4eg.txt";
 const rl = readline.createInterface({
     input: fs.createReadStream(filepath),
     crlfDelay: Infinity
@@ -9,7 +9,21 @@ const rl = readline.createInterface({
 
 const xmasRegex = /(?=XMAS|SAMX)/g
 function countXmas(input) {
-    return [..."XMASAMXAMM".matchAll(xmasRegex)].map(x => x.input.substring(x.index, x.index + 4)).length
+    return [...input.matchAll(xmasRegex)].map(x => x.input.substring(x.index, x.index + 4)).length;
+};
+
+function generateStringsFromVertical(inputArray) {
+    let outputArray = [];
+    let arrayHeight = inputArray.length;
+    let arrayWidth = inputArray[0].length;
+    for(let i = 0; i < arrayWidth; i++) {
+        let generatedString = "";
+        for(let j = 0; j < arrayHeight; j++) {
+            generatedString += inputArray[j].substring(i, i + 1);
+        };
+        outputArray.push(generatedString);
+    }
+    return outputArray;
 }
 
 let counter = 0;
@@ -20,5 +34,11 @@ rl.on('line', (line) => {
 });
 
 rl.on('close', () => {
-
+    console.log(counter);
+    const verticalStrings = generateStringsFromVertical(matrix);
+    console.log(verticalStrings);
+    for(let i = 0; i < verticalStrings.length; i++) {
+        counter += countXmas(verticalStrings[i]);
+    };
+    console.log(counter);
 })
