5f89f3a01d0ef35a578f30793aa9151262aa1515
halmass822 <93576140+halmass822@users.noreply.github.com>
2024-12-09 18:58:39 +0000

    diagonal top left to bottom left

--- 4/4.js
+++ 4/4.js
@@ -26,6 +26,24 @@ function generateStringsFromVertical(inputArray) {
     return outputArray;
 }
 
+function generateStringsFromDiagonal(inputArray) {
+    //first index will be 3, ends at array width - 4
+    //vertical index will -- until 0, horizontal index will ++ until it equals starting vertical index
+    let outputArray = [];
+    let arrayHeight = inputArray.length;
+    let arrayWidth = inputArray[0].length;
+    //top left to bottom left
+    for(let i = arrayHeight - 1; i >= 3; i--) {
+        let generatedString = "";
+        for(let j = 0; j <= i; j++) {
+            generatedString += inputArray[i - j].substring(j,j + 1);
+        }
+        console.log(generatedString);
+        outputArray.push(generatedString);
+    };
+    
+}
+
 let counter = 0;
 let matrix = [];
 rl.on('line', (line) => {
@@ -36,6 +54,7 @@ rl.on('line', (line) => {
 rl.on('close', () => {
     console.log(counter);
     const verticalStrings = generateStringsFromVertical(matrix);
+    const diagonalStrings = generateStringsFromDiagonal(matrix);
     console.log(verticalStrings);
     for(let i = 0; i < verticalStrings.length; i++) {
         counter += countXmas(verticalStrings[i]);
